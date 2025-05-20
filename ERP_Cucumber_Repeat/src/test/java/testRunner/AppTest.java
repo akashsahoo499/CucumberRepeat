8ad005396fe8e10629bcab9ce909a9a3270a22a9
akash <akash@Babu>
2025-05-20 18:50:34 +0530

    First Commit

---
+++ ERP_Cucumber_Repeat/src/test/java/testRunner/AppTest.java
@@ -0,0 +1,17 @@
+package testRunner;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import cucumber.api.testng.AbstractTestNGCucumberTests;
+
+//@RunWith(Cucumber.class)
+@CucumberOptions(features= {"FeatureFiles"},monochrome=true,glue = {"stepDefinition"},tags=("@Supplier,@Customer"),
+		plugin={"pretty","html:target/report/cucumber",
+				"json:target/report/cucumber.json",
+				"junit:target/report/cucumber.xml",
+})
+public class AppTest extends AbstractTestNGCucumberTests{
+
+}
